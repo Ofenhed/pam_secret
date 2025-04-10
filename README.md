72f0b5a66d01c29ed831d9ce544dd70284b5d4cf
Marcus Ofenhed <marcus@conditionraise.se>
2025-04-10 23:15:12 +0200

    Fixes and improvements

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ auth        sufficient              pam_unix.so {if not "without-nullok":nullok}
 After:
 ```
 auth        [success=ok default=1 ignore=ignore]              pam_unix.so {if not "without-nullok":nullok}
-auth        [success=done]                                    pam_tpm2.so pcr_register=23
+auth        [success=done]                                    pam_tpm2.so pcr_23=user
 ```
 
 Preferably you should also run `tpm2_pcrreset` on your register on screen saver activation.
--- pam_tpm2.c
+++ pam_tpm2.c
@@ -15,13 +15,15 @@ PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const ch
 }
 
 PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-    printf("Acct mgmt\n");
     return PAM_SUCCESS;
 }
 
 #define ARR_LEN(x) (sizeof(x)/sizeof(x[0]))
+#ifndef SUDO_BIN
+#define SUDO_BIN "/usr/bin/sudo"
+#endif
 
-static int exec_blocking(const char *prog, char *const argv[]) {
+static int exec_blocking(const char *prog, const char *argv[]) {
     pid_t child = fork();
     if (child == -1) {
         perror("Fork failed");
@@ -37,21 +39,24 @@ static int exec_blocking(const char *prog, char *const argv[]) {
         }
     } else {
         fclose(stdin);
-        if (execve(prog, argv, NULL) == -1) {
+        if (execve(prog, (char *const *) argv, NULL) == -1) {
             perror("Child process execve failed");
             return -1;
         }
     }
 }
 
+static int tpm_function(const char *sudoUser, const char *exec, const char *argument) {
+    if (sudoUser == NULL) {
+        sudoUser = "tss";
+    }
+    const char *tpmArgs[] = {SUDO_BIN, "-u", sudoUser, "--", exec, argument, NULL};
+    return exec_blocking(SUDO_BIN, tpmArgs);
+}
+
 /* expected hook, this is where custom stuff happens */
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
     int retval;
-
-    unsigned int pcr_register = 25;
-    for (int i = 0; i < argc; ++i) {
-        sscanf(argv[i], "pcr_register=%u", &pcr_register);
-    }
 
     const char* pUsername;
     retval = pam_get_user(pamh, &pUsername, NULL);
@@ -60,6 +65,24 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
         return retval;
     }
     const size_t usernameLen = strlen(pUsername);
+
+    const char *sudoUser = NULL;
+    int pcrRegister = -1;
+    int tmpRegister;
+    int count;
+    for (int i = 0; i < argc; ++i) {
+        int separator = -1;
+        if (sscanf(argv[i], "pcr_%u=%n", &tmpRegister, &separator) && separator != -1) {
+            if (strcmp(argv[i] + separator, pUsername) == 0) {
+                pcrRegister = tmpRegister;
+            }
+        } else if (sscanf(argv[i], "as_user=%n", &separator) == 0 && separator != -1) {
+            sudoUser = argv[i] + separator;
+        }
+    }
+    if (pcrRegister == -1) {
+        return PAM_CRED_INSUFFICIENT;
+    }
 
     const char* pAuthToken;
     retval = pam_get_authtok(pamh, PAM_AUTHTOK, &pAuthToken, NULL);
@@ -77,32 +100,30 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
     assert(hashLen == 32);
     if (pCredHash == NULL) {
         fprintf(stderr, "HMAC failed\n");
-        return -1;
+        return PAM_SUCCESS;
     }
     const char tpm_hash[] = "TPM PCR Hash";
     const unsigned char* pResultHash = HMAC(EVP_sha256(), pCredHash, hashLen, tpm_hash, ARR_LEN(tpm_hash), (char*)&resultHash, &hashLen);
     assert(hashLen == 32);
     if (pResultHash == NULL) {
         fprintf(stderr, "HMAC failed\n");
-        return -1;
+        return PAM_SUCCESS;
     }
     const unsigned char hashOutput[65];
     for (int i = 0; i < 32; ++i) {
         snprintf((char*)(&hashOutput[i<<1]), 3, "%02x", pResultHash[i]);
     }
     char outputBuf[128];
-    snprintf((char*)&outputBuf, ARR_LEN(outputBuf), "%u", pcr_register);
+    snprintf((char*)&outputBuf, ARR_LEN(outputBuf), "%u", pcrRegister);
 
-    char *const resetArgs[] = {"tpm2_pcrreset", (char*)&outputBuf, NULL};
-    if (exec_blocking("/usr/bin/tpm2", resetArgs) == -1) {
+    if (tpm_function(sudoUser, "/usr/bin/tpm2_pcrreset", (char*)&outputBuf) == -1) {
         fprintf(stderr, "tpm2_pcrreset failed\n");
-        return -1;
+        return PAM_SUCCESS;
     }
-    snprintf((char*)&outputBuf, ARR_LEN(outputBuf), "%u:sha256=%s", pcr_register, hashOutput);
-    char *const extendArgs[] = {"tpm2_pcrextend", (char*)&outputBuf, NULL};
-    if (exec_blocking("/usr/bin/tpm2", extendArgs) == -1) {
+    snprintf((char*)&outputBuf, ARR_LEN(outputBuf), "%u:sha256=%s", pcrRegister, hashOutput);
+    if (tpm_function(sudoUser, "/usr/bin/tpm2_pcrextend", outputBuf) == -1) {
         fprintf(stderr, "tpm2_pcrextend failed\n");
-        return -1;
+        return PAM_SUCCESS;
     }
 
     return PAM_SUCCESS;
