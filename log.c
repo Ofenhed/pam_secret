d0a8b52a484e4c1e64a01a2809f4548f46bd0004
Marcus Ofenhed <marcus@conditionraise.se>
2025-10-30 19:40:47 +0100

    Added tiered logging

--- log.c
+++ log.c
@@ -1,16 +1,56 @@
+#include "log.h"
+#include "utils.h"
+
+#undef ADD_LOGGER
+
+int __log_default_log_output = 2;
+void set_default_log_output(int fd) { __log_default_log_output = fd; }
+
 #define ADD_LOGGER(LEVEL)                                                      \
+  void vdlog_##LEVEL(int output, const char *restrict format, va_list args) {  \
+    dprintf(output, "%s: ", STRINGIZE_VALUE_OF(LEVEL));                        \
+    vdprintf(output, format, args);                                            \
+    dprintf(output, "\n");                                                     \
+  }                                                                            \
+  void dlog_##LEVEL(int output, const char *restrict format, ...) {            \
+    va_list args;                                                              \
+    va_start(args, format);                                                    \
+    vdlog_##LEVEL(output, format, args);                                       \
+    va_end(args);                                                              \
+  }                                                                            \
+  void vflog_##LEVEL(FILE *output, const char *restrict format,                \
+                     va_list args) {                                           \
+    vdprintf(fileno(output), format, args);                                    \
+  }                                                                            \
+  void flog_##LEVEL(FILE *output, const char *restrict format, ...) {          \
+    va_list args;                                                              \
+    va_start(args, format);                                                    \
+    vflog_##LEVEL(output, format, args);                                       \
+    va_end(args);                                                              \
+  }                                                                            \
+  void vlog_##LEVEL(const char *restrict format, va_list args) {               \
+    vdlog_##LEVEL(__log_default_log_output, format, args);                     \
+  }                                                                            \
   void log_##LEVEL(const char *restrict format, ...) {                         \
     va_list args;                                                              \
     va_start(args, format);                                                    \
-    vfprintf(stderr, ##LEVEL format "\n", args);                               \
-    va_end(args)                                                               \
+    vlog_##LEVEL(format, args);                                                \
+    va_end(args);                                                              \
   }
-#ifdef DEBUG
+#ifndef DEBUG
 #undef ADD_LOGGER
 #define ADD_LOGGER(LEVEL)                                                      \
-  inline void log_##LEVEL(const char *restrict format, ...) { return; }
+  inline void flog_##LEVEL(FILE *output, const char *restrict format, ...) {   \
+    return;                                                                    \
+  }                                                                            \
+  inline void vflog_##LEVEL(FILE *output, const char *restrict format,         \
+                            va_list args) {                                    \
+    return;                                                                    \
+  }                                                                            \
+  inline void log_##LEVEL(const char *restrict format, ...) { return; }        \
+  inline void vlog_##LEVEL(const char *restrict format, va_list args) {        \
+    return;                                                                    \
+  }
 #endif
 
-ADD_LOGGER(debug)
-ADD_LOGGER(warning)
-ADD_LOGGER(error)
+#include "log_impls.incl"
--- log.h
+++ log.h
@@ -1,9 +1,16 @@
 #pragma once
 
-void log_debug(const char *restrict format, ...);
+#include <stdarg.h>
+#include <stdio.h>
 
-#define ADD_LOGGER(LEVEL) void log_##LEVEL(const char *restrict format, ...);
+void set_default_log_output(int fd);
 
-ADD_LOGGER(debug)
-ADD_LOGGER(warning)
-ADD_LOGGER(error)
+#define ADD_LOGGER(LEVEL)                                                      \
+  void log_##LEVEL(const char *restrict format, ...);                          \
+  void vlog_##LEVEL(const char *restrict format, va_list args);                \
+  void dlog_##LEVEL(int output, const char *restrict format, ...);             \
+  void vdlog_##LEVEL(int output, const char *restrict format, va_list args);   \
+  void flog_##LEVEL(FILE *output, const char *restrict format, ...);           \
+  void vflog_##LEVEL(FILE *output, const char *restrict format, va_list args);
+
+#include "log_impls.incl"
